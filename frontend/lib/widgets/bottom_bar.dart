dcadf50e111832a34d49fd228187a3ae71684282
nhatminh23-03 <nhatdoanminh2303@gmail.com>
2024-09-23 14:42:57 -0700

    Edit bottom bar navigation

--- frontend/lib/widgets/bottom_bar.dart
+++ frontend/lib/widgets/bottom_bar.dart
@@ -5,6 +5,39 @@ class bottom_bar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return NavigationBar(
+      destinations: const [
+        NavigationDestination(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.add_a_photo_outlined),
+          label: 'Upload',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.bar_chart),
+          label: 'Stats',
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.chrome_reader_mode_outlined),
+          label: 'History',
+        ),
+      ],
+      onDestinationSelected: (int index) {
+        // Handle destination tap events
+      },
+    );
+  }
+}
+
+/*import 'package:flutter/material.dart';
+
+class bottom_bar extends StatelessWidget {
+  const bottom_bar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
       return Scaffold(
       bottomNavigationBar: NavigationBar(
         destinations: const[
@@ -28,4 +61,4 @@ class bottom_bar extends StatelessWidget {
       )
        );
   }
-}
+}*/
